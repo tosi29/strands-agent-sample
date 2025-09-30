dc16e1b5f76e15040d9389c61b4e56b6303d4238
tosi29 <tosi16@gmail.com>
2025-10-01 02:10:04 +0900

    PORTKEY経由でアクセスする最小サンプル

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from strands-agent-sample!")
+import os
+from dotenv import load_dotenv
+from strands import Agent
+from strands_tools import calculator
+from strands.models.openai import OpenAIModel
+from portkey_ai import PORTKEY_GATEWAY_URL
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+model = OpenAIModel(
+    client_args={
+        "api_key": os.environ["PORTKEY_API_KEY"],
+        "base_url": PORTKEY_GATEWAY_URL
+    },
+    model_id="@google/gemini-2.5-flash-lite",
+)
+
+agent = Agent(model=model, tools=[calculator])
+result = agent("こんにちは")
